62a6003dd390a9a3738153036290a404e1dc884d
reg <reg@regde-MacBook-Pro.local>
2025-02-24 00:04:59 +0800

    移除移除test測試測試檔案

--- api/test.php
+++
@@ -1,8 +0,0 @@
-<?php include_once "db.php";
-$type=$_POST['type'];
-$rows=$News->all(['type'=>$type]);
-
-
-foreach($rows as $row){
-    echo "<a href='javascript:getPost({$row['id']})' class='list-item'>{$row['title']}</a>";
-}
