5975124b4bb4b236ce8c3db3ab3a1d8962c04d10
reg <reg8740223@gmail.com>
2025-02-27 16:17:07 +0800

    db2練習

--- api/db2.php
+++ api/db2.php
@@ -19,39 +19,95 @@ function __construct($table){
 
 
 
-
 function all(...$arg){
-    $sql="SELECT * FROM $this->table";
-    if(!empty($arg[0])){
-        if(in_array($arg[0])){
-            $where=$this->a2s($arg[0]);
-            $sql=$sql . " WHERE " .join(" && " ,$where );
-        }else{
-            $sql .=$arg[0];
-        }
-    }
-    if(!empty($arg[1])){
-        $sql=$sql . $arg[1];
-    }
- return $this->fetchOne($sql);
+$sql="SELECT * FROM $this->table ";
+if(!empty($arg[0]) && is_array($arg[0])){
+$tmp=$this->arrayToSQL($arg[0]);
+$sql .=" where ".join(" && ",$tmp);
+}else if(isset($arg[0]) && is_string($arg[0])){
+$sql .=$arg[0];
+}
+if(isset($arg[0]) && ! empty($arg[1])){
+    $sql .=$arg[1];
+}
 
+return $this->fetch_all($sql);
+}
+
+
+
+public function find($array){
+    $sql="SELECT * FROM $this->table ";
+if(is_array($array)){
+$tmp=$this->arrayToSQL($array);
+$sql .=" where ".join(" && ",$tmp);
+}else{
+    $sql .=" where `id`='$array'";
+
+}
+return $this->fetch_one($sql);
+}
+
+
+public function save($array){
+if(isset($array['id'])){
+    $id=$array['id'];
+    unset($array['id']);
+$tmp=$this->a2s($array);
+$sql="update $this->table set" . join(",",$tmp) . "where `id`='$id'";
+
+
+}else{
+
+$keys = join("`,`",array_keys(($array)));
+$values=join("','",$array);
+$sql ="insert into $this->table(`{$key}`)  values('{$values}')";
+
+
+}
+
+    return $this->pdo->exec($sql);
 }
 
 
 
 
-function find($id){
 
-    $sql="SELECT * FROM $this->table";
- 
-        if(in_array($id[0])){
-            $where=$this->a2s($id[0]);
-            $sql=$sql . " WHERE " .join(" && " ,$where );
-        }else{
-           $sql .=" WHERE `id`='$id' "; 
-        }
- return $this->fetchOne($sql);
+
+
+
+
+
+
+
+
+public function del($array){
+    $sql="DELETE FROM $this->table ";
+    if(is_array($array)){
+    $tmp=$this->arrayToSQL($array);
+    $sql .=" where ".join(" && ",$tmp);
+    }else{
+        $sql .=" where `id`='$array'";
     
+    }
+    return $this->pdo->exec($sql);
+    }
+
+
+
+public function count(...$arg){
+    $sql="SELECT * FROM $this->table ";
+if(!empty($arg[0]) && is_array($arg[0])){
+$tmp=$this->arrayToSQL($arg[0]);
+$sql .=" where ".join(" && ",$tmp);
+}else if(isset($arg[0]) && is_string($arg[0])){
+$sql .=$arg[0];
+}
+if(empty($arg[1])){
+    $sql .=$arg[1];
+}
+
+return $this->pdo->query($sql)->fetchColumn($sql);
 }
 
 
@@ -60,70 +116,64 @@ function find($id){
 
 
 
-function save(){
 
+
+
+
+
+
+public function arrayToSQL($array){
+
+$tmp=[];
+foreach($array as $key =>$value){
+    $tmp[]="`$key`='$value'";
 }
-
-function del(){
-
+return $tmp;
 }
-
-
-function a2s(){
-
+public function fetch_one($sql){
+    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
-
-
-
-
-
-
-
-protected function math(){
-
-}
-
-function max(){
-
-}
-function min(){
-
-}
-
-function sum(){
-
-}
-
-function avg(){
-
-}
-
-function count(){
-
+public function fetch_all($sql){
+    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
 
 
 
 
 
-function fetchOne(){
 
-}
-function fetchALL(){
-    
-}
 
-function dd(){
+
+
 
 }
 
+
+
+// db外
 function to(){
+header("location:".$url);
 
 }
-function q(){
 
+
+
+
+function dd($array){
+echo "<pre>";
+print_r($array);
+echo "</pre>";
 }
 
+
+
+
+
+function q($sql){
+   $dsn="mysql:host=localhost;charset=utf8;dbname=db22";
+   $pdo=new PDO($dsn,'root','');
+   return $pdo->query($sql)->fetchAll();
+}
 
 
 
